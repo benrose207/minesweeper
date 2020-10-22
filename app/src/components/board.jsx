a0f95dd9011d41a8450d7ffe5677f00952cfe9e1
Ben Rose <benrose207@gmail.com>
2020-10-22 15:48:20 -0700

    update favicon

--- app/src/components/board.jsx
+++ app/src/components/board.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Tile from './tile';
 
-// better thing to use for the indexes here? -- maybe interpolate as string?
-
 const BoardLayout = ({ board, update }) => {
   const tiles = board.map((row, idx1) => (
     <div key={idx1} className="row">
